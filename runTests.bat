4b2987f29090fe3d9ad5a88d8dcc28f534241a52
david-cattermole <cattermole91@gmail.com>
2017-07-02 03:51:58 +0930

    MS Windows support for testing.

---
+++ runTests.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+setlocal
+set ASSETQC_BASE_DIR=D:\Dev\assetQC\
+set ASSETQC_CONFIG_PATH=%ASSETQC_BASE_DIR%\tests\test\data\config\config.json
+REM echo "ASSETQC_BASE_DIR:" ${ASSETQC_BASE_DIR}
+REM echo "ASSETQC_CONFIG_PATH:" ${ASSETQC_CONFIG_PATH}
+
+
+REM run API tests without coverage branches
+nosetests.exe ./tests/test/runTests.py ./python ./tests/test --with-coverage --cover-erase --cover-tests --cover-package=assetQC
+
+REM run API tests with coverage branches (only supported in 'nose 4.x')
+REM nosetests.exe ./tests/test/runTests.py ./python ./tests/test --with-coverage --cover-erase --cover-tests --cover-package=assetQC --cover-branches
+
+REM pause
